5e82f5effec8ba724789d0cb8019ebe79e29a124
JT <jack.turns@hotmail.com>
2022-09-21 20:49:55 +0930

    new

--- home.php
+++ home.php
@@ -20,15 +20,28 @@
 <!--Product cards-->
 <!--php code to create card element for top n items in database-->
 <?php
+require_once "scripts/dbconnect.php";
+$sql = "SELECT * from ItemStock";
+global $conn;
 //currently loops 10 times echoing the same html
 //needs database conn, retrieve top n items, use vairbales to change price, name, image and text fields
-for ($x = 0; $x <= 10; $x++) {
-    echo '<div class="card"> <img src="images/test.png"> <h1>product name</h1>
-<p class="price">$19.99</p>
-<p>Some text about the product..</p>
-<p><button>Add to Cart</button></p>
-</div>';
+
+if($result = mysqli_query($conn, $sql)){
+    $num_rows = mysqli_num_rows($result);
+    while($row = mysqli_fetch_assoc($result)) {
+        $code = $row['item_code'];
+        $name = $row['item_name'];
+        $price = $row['price'];
+        $descr = $row['description'];
+        $bytes = $row['image_bytes'];
+        echo "<div class='card'> <img src='data:image/jpeg;base64,$bytes'> <h1>" . $name . "</h1><p class='price'>$". $price . "</p>
+        <p><button>Add to Cart</button></p>
+        </div>";
+    }
+
 }
+
+
 ?>
 <!-- Base card code, now used in php loop
 <div class="card"> <img src="images/test.png">
