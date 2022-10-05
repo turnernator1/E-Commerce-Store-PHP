6f946d5023d521ce6ca9a8d6796f2c41233c52c6
turnernator1 <turn0438@flinders.edu.au>
2022-10-05 18:27:45 +1030

    Added login.php

--- scripts/login.php
+++ scripts/login.php
@@ -13,7 +13,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $username = $_POST["username"];
     $password = $_POST["password"];
-
     $hash = password_hash($password, PASSWORD_DEFAULT);
 
 
