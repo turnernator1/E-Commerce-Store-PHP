79e17b5f691bf1546797adfacefb7ab38d1d614c
turnernator1 <turn0438@flinders.edu.au>
2022-10-05 18:27:22 +1030

    Added login.php

--- scripts/login.php
+++ scripts/login.php
@@ -13,6 +13,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $username = $_POST["username"];
     $password = $_POST["password"];
+
     $hash = password_hash($password, PASSWORD_DEFAULT);
 
 
