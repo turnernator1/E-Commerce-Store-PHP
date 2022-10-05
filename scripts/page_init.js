0082cb10d9eebdba5d98fdafee99f72ccde01b85
turnernator1 <turn0438@flinders.edu.au>
2022-10-05 20:33:01 +1030

    Dynamic login debug

--- scripts/page_init.js
+++ scripts/page_init.js
@@ -6,7 +6,15 @@
 const signin = document.getElementById("signin");
 
 if (get_username() != 0){
-    signin.innerHTML = '<a href="account.php"> <b>My Account</b></a>'
+    //signin.innerHTML = '<a href="account.php"> <b>My Account</b></a>'
+    signin.innerHTML = '<a href="account.php"> <div class="dropdown">\n' +
+        '  <button class="dropbtn">My Account</button>\n' +
+        '  <div class="dropdown-content">\n' +
+        '    <a href="#">Link 1</a>\n' +
+        '    <a href="#">Link 2</a>\n' +
+        '    <a href="#">Link 3</a>\n' +
+        '  </div>\n' +
+        '</div></a>'
 } else {
     signin.innerHTML ='<a href="signin.php"> <b>Sign In / Register </b></a>'
 }
