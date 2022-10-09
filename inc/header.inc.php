e895b87226cb2247c5f1322f162d54183928fd44
turnernator1 <turn0438@flinders.edu.au>
2022-10-09 13:23:58 +1030

    Profile page working

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -6,7 +6,7 @@ I have also added a hidden class as certain features need to be hidden for the l
 
 
 <div class="topnav">
-    <img src="img/logo.svg" alt="Logo" class="logo">
+    <a href="home.php"><img src="img/logo.svg" alt="Logo" class="logo"></a>
     <a href="home.php"><b class="active">Home</b></a>
     <div class="">
     <a href="home.php"><b>Shop</b></a>
