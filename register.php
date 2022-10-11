e55fce53e98063481868633f54e4d32a53800f00
Leo Fuentes <leo.emile.fuentes@gmail.com>
2022-10-11 22:00:50 +1030

    fixed register

--- register.php
+++ register.php
@@ -22,40 +22,44 @@
     <div class="AccountPage">
         <div class="Container">
             <div class="row">
-                <div class="FormContainer registration_form">
+                <div id="regFormCont" class="FormContainer registration_form">
                     <div class="FormBTN">
                         <!-- <span> -->
-                        <h2>Enter your Details To Join</h2>
+                        <h2 id="signupTitle">Enter your Details To Join</h2>
                         <br>
-                        <h4>Registering will only take a minute!</h4>
+
                         <br>
                         <!-- </span> -->
-                        <form action="scripts/signup.php" method="post">
-                            <input name="username" type="text" placeholder="Username" required>
-                            <select name="title">
+                        <form id="signupForm" action="scripts/signup.php" method="post">
+                            <div id="regSubHead"> Username: </div>
+                            <input id="signupField" name="username" type="text" placeholder="Username" required> <br>
+                            <div id="regSubHead"> First Name: </div>
+
+                            <select id="signupSelect" name="title">
                                 <option value="Mr">Mr</option>
                                 <option value="Mrs">Mrs</option>
                                 <option value="Ms">Ms</option>
                                 <option value="-">Other</option>
                             </select>
-                            <span class="sideBySide">
-                                <input name="preferred" class="left" type="text" placeholder="First name" required>
-                                <input name="surname" class="right" type="text" placeholder="Last name" required>
-                            </span>
-                            <input name="email" type="email" placeholder="Email address" required>
-                            <div class="tooltip">Tips for a secure password?
+                                <input id="signupField" name="preferred" type="text" placeholder="First name" required> <br>
+                                <div id="regSubHead"> Surname: </div>
+                                <input id="signupField" name="surname" type="text" placeholder="Last name" required><br>
+                                <div id="regSubHead"> Email: </div>
+                                <input id="signupField" name="email" type="email" placeholder="Email address" required><br>
+                                <div id="regSubHead"> Password: </div>
+                                <input id="signupField" name="password" type="password" placeholder="Password" required> <br>
+                                <div id="regSubHead"> Confirm Password: </div>
+                                <input id="signupField"  name="cpassword" type="password" placeholder="Confirm Password" required><br>
+                            <div class="tooltip">Tips for a secure password? <br>
                                 <span class="tooltiptext">
                                 <ul>
                                     <li>Include numbers, symbols, and both uppercase and lowercase.</li>
                                     <li>No personal details.</li>
                                     <li>Use a unique password for every site.</li>
                                 </ul>    
-                                </span>
                             </div>
-                            <input name="password" type="password" placeholder="Password" required>
-                            <input name="cpassword" type="password" placeholder="Confirm Password" required>
-
-                            <button type="submit" class="BTN login_button register_button">Next</button>
+                            <br>
+                            <button id="signupButton" type="submit" class="BTN login_button register_button">Next</button>
                         </form>
                     </div>
 
